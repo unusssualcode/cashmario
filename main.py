28270d543624efe09d801df4439bbd756d29e075
Denis <densmm@ukr.net>
2025-10-03 10:48:44 +0300

    Added lost screen and restart game func

--- main.py
+++ main.py
@@ -52,8 +52,12 @@ mushroom_timer = pygame.USEREVENT + 1
 # pygame.time.set_timer(mushroom_timer, 1000)
 pygame.time.set_timer(mushroom_timer, random.randint(1000, 5000))
 
+label = pygame.font.Font("fonts/Mont-Bold.ttf", 40)
+lose_label = label.render("You lost", False, (193, 196, 199))
+restart_label = label.render("Start new game", False, (0, 255, 0))
+restart_label_rect = restart_label.get_rect(topleft=(180, 200))
 
-gameplay = 
+gameplay = True
 
 """
 Screen show game
@@ -64,56 +68,68 @@ while running:
     screen.blit(bg, (bg_x, 0))
     screen.blit(bg, (bg_x + 800, 0))
 
-    
-    player_rect = walk_left[0].get_rect(topleft=(player_x, player_y))
-    # mushroom_rect = mushroom.get_rect(topleft=(mushroom_x, 320))
-    
-    if mushroom_list_in_game:
-        for el in mushroom_list_in_game:
-            screen.blit(mushroom, el)
-            el.x -= 10
-    
-            if player_rect.colliderect(el):
-                print("You lost")
-    
-    
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        screen.blit(walk_left[player_anim_count], (player_x, player_y))
-    else:
-        screen.blit(walk_right[player_anim_count], (player_x, player_y))
-    
-    
-    if keys[pygame.K_LEFT] and player_x > 50:
-        player_x -= player_speed
-    elif keys[pygame.K_RIGHT] and player_x < 150:
-        player_x += player_speed
+    if gameplay:
+        player_rect = walk_left[0].get_rect(topleft=(player_x, player_y))
+        # mushroom_rect = mushroom.get_rect(topleft=(mushroom_x, 320))
+        
+        if mushroom_list_in_game:
+            for (i,el) in enumerate(mushroom_list_in_game):
+                screen.blit(mushroom, el)
+                el.x -= 10
+                
+                if el.x < -10:
+                    mushroom_list_in_game.pop(i)
+        
+                if player_rect.colliderect(el):
+                    gameplay = False
         
         
-    if not is_jumping:
-        if keys[pygame.K_SPACE]:
-            is_jumping = True
-    else:
-        if jump_count >= -10:
-            if jump_count > 0:
-                player_y -= (jump_count ** 2) / 2
-            else:
-                player_y += (jump_count ** 2) / 2
-            jump_count -= 1
-            
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            screen.blit(walk_left[player_anim_count], (player_x, player_y))
         else:
-            is_jumping = False
-            jump_count = 10
+            screen.blit(walk_right[player_anim_count], (player_x, player_y))
+        
+        
+        if keys[pygame.K_LEFT] and player_x > 50:
+            player_x -= player_speed
+        elif keys[pygame.K_RIGHT] and player_x < 150:
+            player_x += player_speed
+            
+            
+        if not is_jumping:
+            if keys[pygame.K_SPACE]:
+                is_jumping = True
+        else:
+            if jump_count >= -10:
+                if jump_count > 0:
+                    player_y -= (jump_count ** 2) / 2
+                else:
+                    player_y += (jump_count ** 2) / 2
+                jump_count -= 1
+                
+            else:
+                is_jumping = False
+                jump_count = 10
 
-    if player_anim_count == 3:
-        player_anim_count = 0
+        if player_anim_count == 3:
+            player_anim_count = 0
+        else:
+            player_anim_count += 1
+
+        bg_x -= 2
+        if bg_x == -800:
+            bg_x = 0
     else:
-        player_anim_count += 1
-
-    bg_x -= 2
-    if bg_x == -800:
-        bg_x = 0
-
+        screen.fill((87, 87, 87))
+        screen.blit(lose_label, (180, 100))
+        screen.blit(restart_label, restart_label_rect)
+        
+        mouse = pygame.mouse.get_pos()
+        if restart_label_rect.collidepoint(mouse) and pygame.mouse.get_pressed()[0]:
+            gameplay=True
+            player_x = 150
+            mushroom_list_in_game.clear()
     
     pygame.display.update()
     """
